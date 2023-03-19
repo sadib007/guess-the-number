88dafa1f031a9b238440a93000d645a6a499c72c
sadib007 <128297951+sadib007@users.noreply.github.com>
2023-03-19 14:59:58 +0100

    added timer

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,15 +1,22 @@
 import random
-
+from datetime import datetime, timedelta
 
 # returns the random integer between x and y including x and y
+
+
 def generateRandomNumber(x: int, y: int) -> int:
     return random.randint(x, y)
+
+
+def printTimer(inception: datetime) -> str:
+    now = datetime.now()
+    return (now-inception).seconds
 
 
 random_number = generateRandomNumber(1, 100)
 attempt = 0  # attempt counter
 user_input = 0  # varraible to hold user input
-
+start_time = datetime.now()
 while (random_number != user_input):
     x = input("Please enter the number you guess. Enter q to quit the game: ")
     if (x.lower() == 'q'):  # control to check if user decided to quit
@@ -17,15 +24,18 @@ while (random_number != user_input):
     try:
         user_input = int(x)  # convert entered user input from string to int
     except ValueError as e:  # catch exception if user enters anything else other than an integer number
-        print("The entered input is not an integer number. Please enter an integer number.")
+        print(
+            f"The entered input is not an integer number. Please enter an integer number. Elapsed Time: {printTimer(start_time)}s")
         continue
     attempt += 1    # increase attempt counter
     if user_input > random_number:
         # entered number is higher than the guess
-        print("Your guess is too high.")
+        print(
+            f"Your guess is too high. Elapsed Time: {printTimer(start_time)}s")
     else:
         # entered number is lower than the guess.
-        print("Your guess is too low.")
+        print(
+            f"Your guess is too low. Elapsed Time: {printTimer(start_time)}s")
 
 print(
-    f"You needed {attempt} attemps to guess the correct number {random_number}.")  # final result
+    f"You needed {attempt} attemps to guess the correct number {random_number}. Elapsed Time: {printTimer(start_time)}s")  # final result
